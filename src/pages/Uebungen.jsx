7037281376a370116510a58de38111a5e652e056
BradyThe9 <boehmebruno123@gmail.com>
2025-05-17 13:46:46 +0200

    fix: Default-Export in Uebungen.jsx ergänzt

---
+++ src/pages/Uebungen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Uebungen() {
+  return (
+    <div>
+      <h1>Übungen</h1>
+      <p>Hier stehen deine Übungsaufgaben …</p>
+    </div>
+  )
+}
+
+export default Uebungen
